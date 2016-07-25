5d0d9325ed95e54595d6fcf117a7d2baf322d35c
NaMLu <osahin@outlook.com>
2016-07-25 14:29:47 +0300

    Ders 03

---
+++ AnaApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AnaappConfig(AppConfig):
+    name = 'AnaApp'
---
+++ AnaApp/views.py
@@ -0,0 +1,7 @@
+﻿from django.shortcuts import render
+
+# Create your views here.
+def anasayfa(request):
+	title = "Ana sayfa"
+	
+	return render(request, "anasayfa.html", locals())
